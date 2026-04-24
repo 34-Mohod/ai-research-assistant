1ef22b6204270c4df8cedcb74f179b7ea8d24c09
safalya vijay mohod <safalyavijaymohod@MacBookAir.lan>
2026-04-24 09:19:32 +0530

    switch to supported model

--- modules/llm_engine.py
+++ modules/llm_engine.py
@@ -39,7 +39,7 @@ TEXT:
 
     try:
         response = client.chat.completions.create(
-            model = "llama3-8b-8192",
+            model = "llama3-70b-8192",
             messages=[{"role": "user", "content": prompt}],
             temperature=0.2
         )
